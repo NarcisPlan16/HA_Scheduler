fb7c890985b852e017a741a6985a3af16483df52
Narcís Planellas <u1972817@campus.udg.edu>
2024-02-19 12:11:32 +0100

    dev

--- optimal_scheduler/run.sh
+++ optimal_scheduler/run.sh
@@ -1,21 +1,17 @@
-#!/usr/bin/with-contenv bashio
-
+#!/usr/bin/env bashio
+#
 # ==============================================================================
 # Start the example service
 # s6-overlay docs: https://github.com/just-containers/s6-overlay
 # ==============================================================================
-
+#
 # Add your code here
-
-# Declare variables
-declare message
-
+#
 ## Get the 'message' key from the user config options.
 message=$(bashio::config 'message')
-
 ## Print the message the user supplied, defaults to "Hello World..."
 bashio::log.info "${message:="Hello World..."}"
-
+#
 ## Run your program
 #exec /usr/bin/my_program
 #python Abstraction/main.py
