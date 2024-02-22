cb66c05711e561492d13ccc845332c6e9a18c521
Narcís <narcisplan@gmail.com>
2024-02-22 11:57:55 +0100

    dev

---
+++ optimal_scheduler/findBashio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#cd /usr/lib
+#ls -l | grep bashio
+
+#command -v bashio
+sudo find / -name bashio
--- optimal_scheduler/run.sh
+++ optimal_scheduler/run.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/with-contenv bashio
 
-find / -name "bashio" -type f 2>/dev/null
-
-if command -v bashio &> /dev/null; then
-    echo "bashio is in the PATH"
-else
-    echo "bashio is not in the PATH"
-    exit 1
-fi
-
 message=$(bashio::config 'message')
 bashio::log.info "${message:="Hello World..."}"
