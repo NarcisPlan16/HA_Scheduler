5c4d1e322c62f88949042b5fe0b2cab7deccf5fc
Narcís <narcisplan@gmail.com>
2024-03-19 18:44:46 +0100

    dev

--- optimal_scheduler/rootfs/etc/services.d/example/Abstraction/main.py
+++ optimal_scheduler/rootfs/etc/services.d/example/Abstraction/main.py
@@ -17,7 +17,7 @@ import OptimalScheduler as optimalscheduler
 # URL for the Home Assistant API
 # TODO: WORK WITH .secrets
 ha_url = "http://192.168.1.192:8123"
-bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiOWUzNjU4NWVkMzI0YzYxYWFlYTdhMmZiZTkyNGY0MCIsImlhdCI6MTcwNzMwMjM1OCwiZXhwIjoyMDIyNjYyMzU4fQ.d-brZLxCDdcUtuf5XpOjWjCBd-q4gPBgc18B7skr6z8"
+bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkYjcxOTI3NmM2ZTA0YzU5YTZmM2YxZmFlOTUxZWM5OSIsImlhdCI6MTcxMDg2Nzc4NywiZXhwIjoyMDI2MjI3Nzg3fQ.72uuDLPBzDVVX7enOXmDlvI-eDcQxU_wPgAeHqw6eGs"
 ha = Client(ha_url, bearer_token) # Connect to Home Assistant instance
 
 def backgroundSimulation(gui, os):
@@ -69,10 +69,28 @@ if __name__ == "__main__":
     message = str(sys.argv[1])
 
     print(message)
-    print(ha.get_entity(entity_id="sun.sun").get_state()) #sensor.dht1_humidity
+    #print(ha.get_error_log()) #get_entity(entity_id="sensor.dht1_humidity")
 
+headers = {
+    "Authorization": f"Bearer {bearer_token}",
+    "Content-Type": "application/json",
+}
 
+sensor_entity_id = "sensor.dht1_humidity"
+# Make a GET request to retrieve the state of the sensor
+response = requests.get(f"http://supervisor/core/api/states/{sensor_entity_id}", headers=headers)
 
-
-
+if response.status_code == 200:
+    try:
+        # Try to decode the JSON response
+        sensor_state = response.json()["state"]
+        print(f"The current state of {sensor_entity_id} is: {sensor_state}")
+    except json.JSONDecodeError as e:
+        # If response is not JSON, print the response content
+        print("Response is not in JSON format:")
+        print(response.text)
+        print("JSONDecodeError:", e)
+else:
+    # Print error message if request was not successful
+    print(f"Failed to retrieve state for sensor {sensor_entity_id}. Status code: {response.status_code}")
 
