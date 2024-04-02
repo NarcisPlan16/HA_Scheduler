5c93b27e2cd2165329b230d6e8d72e8ca6944fad
Narcís <narcisplan@gmail.com>
2024-04-02 13:51:02 +0200

    Now the main.py code interacts with the api, sending a request for all entities and then checking that all the inputed entities by the user exist.

--- optimal_scheduler/rootfs/etc/services.d/example/Abstraction/main.py
+++ optimal_scheduler/rootfs/etc/services.d/example/Abstraction/main.py
@@ -16,7 +16,7 @@ from flask import Flask
 
 # URL for the Home Assistant API
 # TODO: WORK WITH .secrets
-ha_url = "http://192.168.0.117:8123"
+ha_url = "http://192.168.1.192:8123"
 bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkYjcxOTI3NmM2ZTA0YzU5YTZmM2YxZmFlOTUxZWM5OSIsImlhdCI6MTcxMDg2Nzc4NywiZXhwIjoyMDI2MjI3Nzg3fQ.72uuDLPBzDVVX7enOXmDlvI-eDcQxU_wPgAeHqw6eGs"
 
 def backgroundSimulation(gui, os):
@@ -38,18 +38,38 @@ def checkloop(event: threading.Event):
 
         time.sleep(2)
 
-def importConfiguration():
 
-    config = read_options()
+def checkConsumers(entity_ids):
 
-    for asset_type in config:
-        for asset_class in config[asset_type]:
-            for asset in config[asset_type][asset_class].values():
+    if str(sys.argv[2]): # the string is not empty
 
-                scheduler.addAsset(asset_type, asset_class, asset)
+        consumers = str(sys.argv[2]).split("\n") # Convert the inputed consumers string into an array. They must be separated by enlines (\n)
+        for consumer in consumers:
+            if consumer not in entity_ids:
+                print(f"[ERROR]: Consumer {consumer} not found in consumer ist. Check the consumer ID")
+                break
 
+def checkGenerators(entity_ids):
 
-def read_options():
+    if str(sys.argv[3]): # the string is not empty
+
+        generators = str(sys.argv[3]).split("\n") # Convert the inputed consumers string into an array of strings. They must be separated by enlines (\n)
+        for generator in generators:
+            if generator not in entity_ids:
+                print(f"[ERROR]: Generator {generator} not found in generators ist. Check the generator ID")
+                break
+
+def checkEnergySources(entity_ids):
+
+    if str(sys.argv[4]): # the string is not empty
+
+        esources = str(sys.argv[4]).split("\n") # Convert the inputed consumers string into an array. They must be separated by enlines (\n)
+        for esource in esources:
+            if esource not in entity_ids:
+                print(f"[ERROR]: Energy Source {esource} not found in energy sources ist. Check the energy source ID")
+                break
+
+def checkBuilding(entity_ids):
     pass
 
 
@@ -64,38 +84,40 @@ if __name__ == "__main__":
     #event = threading.Event()
     scheduler = optimalscheduler.OptimalScheduler()
 
-    #importConfiguration()
-
     #write_pipe = multiprocessing.Pipe()
     #t2 = multiprocessing.Process(target=scheduler.startOptimizationNoPipe)#, args=(write_pipe,))
     #t2.start()
 
-    #read_options()
-    message = str(sys.argv[2])
+    headers = {
+        "Authorization": f"Bearer {bearer_token}", #str(sys.argv[1]) for SUPERVISED_TOKEN
+        "Content-Type": "application/json",
+    }
 
-    print(message)
-    #print(ha.get_error_log()) #get_entity(entity_id="sensor.dht1_humidity")
+    sensor_entity_id = "sensor.dht1_humidity"
+    # Make a GET request to retrieve the state of the sensor
+    response = requests.get(f"{ha_url}/api/states", headers=headers) #http://supervisor/core/api/states/{sensor_entity_id} 
+    
+    if response.status_code == 200:
+        try:
 
-headers = {
-    "Authorization": f"Bearer {bearer_token}", #str(sys.argv[1]) for SUPERVISED_TOKEN
-    "Content-Type": "application/json",
-}
+            entity_ids = [entity["entity_id"] for entity in response.json()] # Extract only the entity ids
+            #for entity in entity_ids:
+            #    print(entity)
 
-sensor_entity_id = "sensor.dht1_humidity"
-# Make a GET request to retrieve the state of the sensor
-response = requests.get(f"{ha_url}/api/states/{sensor_entity_id}", headers=headers) #http://supervisor/core/api/states/{sensor_entity_id} 
+            print(str(sys.argv[2]))
+            checkConsumers(entity_ids)
+            checkGenerators(entity_ids)
+            checkEnergySources(entity_ids)
+            checkBuilding(entity_ids)
 
-if response.status_code == 200:
-    try:
-        # Try to decode the JSON response
-        sensor_state = response.text#.json() #["state"]
-        print(f"The current state of {sensor_entity_id} is: {sensor_state}")
-    except json.JSONDecodeError as e:
-        # If response is not JSON, print the response content
-        print("Response is not in JSON format:")
-        print(response.text)
-        print("JSONDecodeError:", e)
-else:
-    # Print error message if request was not successful
-    print(f"Failed to retrieve state for sensor {sensor_entity_id}. Status code: {response.status_code}")
+            print("[DEBUG]: All entities found!")
+
+        except json.JSONDecodeError as e:
+            # If response is not JSON, print the response content
+            print("Response is not in JSON format:")
+            print(response.text)
+            print("JSONDecodeError:", e)
+    else:
+        # Print error message if request was not successful
+        print(f"Failed to retrieve state for sensor {sensor_entity_id}. Status code: {response.status_code}")
 
