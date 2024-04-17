d11413b067ea6ea11e5d9935f02c19a25e04c5de
Narcís Planellas <u1972817@campus.udg.edu>
2024-04-17 14:32:01 +0200

    data preprocessing almost done

--- Forecasters/GenerationForecaster/main.py
+++ Forecasters/GenerationForecaster/main.py
@@ -1,4 +1,6 @@
 import requests
+import pandas as pd
+from datetime import datetime
 
 ha_url = "http://192.168.0.110:8123"
 bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJlYzZhYjAxYTVkM2M0OGE3YjU0OGQ1NmYxNjQyNWQ2ZCIsImlhdCI6MTcxMzM1MDQxNSwiZXhwIjoyMDI4NzEwNDE1fQ.Eutl8pls09_KCIWESOv17gmIzu-RW32eazbHp2V4Wr0"
@@ -8,20 +10,29 @@ headers = {
     "Content-Type": "application/json",
 }
 
-entity = "sensor.solarnet_power_photovoltaics"
-ini = "2023-01-01"
+entity = "sensor.sonnenbatterie_79259_meter_production_4_1_w_total"
+ini = "2023-08-01"
 end = "2024-04-16"  # Year - month - Day
 response = requests.get(f"{ha_url}/api/history/period/"+ini+"T00:00:00?end_time="+end+"T00:00:00&filter_entity_id="+entity, headers=headers)
 
 if response.status_code == 200:
-    data = response.json()
 
-    # Extracting only the "state" variables
-    states = []
-    for entry in data:
-        states.append((entry['last_updated'], entry['state']))
+    response_data = response.json()[0]
+    data = pd.DataFrame()
+    data = data.from_dict(response_data)
+    columns = ['last_updated', 'state']
 
-    print(states)
+    # Drop columns except the desired ones
+    columns_to_drop = [col for col in data.columns if col not in columns]
+    data = data.drop(columns=columns_to_drop, axis=1)
+    data['state'] = data['state'].apply(pd.to_numeric, errors='coerce')
+    data['last_updated'] = data['last_updated'].str.split('+').str[0]
+    data['last_updated'] = data['last_updated'].apply(datetime.fromisoformat)
+
+    data = data.set_index('last_updated')
+    data = data.resample('1s').mean().ffill().resample('1h').mean()
+
+    print(data)
 
 else:
     print(response.text)
