8bf71b4983dc724448bde633dea1330041f9cf32
Narcís Planellas <u1972817@campus.udg.edu>
2024-05-07 12:52:44 +0200

    Started PV refactoring

--- optimal_scheduler/rootfs/etc/services.d/example/Abstraction/Asset_types/Generators/PV/PV.py
+++ optimal_scheduler/rootfs/etc/services.d/example/Abstraction/Asset_types/Generators/PV/PV.py
@@ -1,9 +1,14 @@
 # Photovoltaic class for photovoltaic generators
 
-from Generator import Generator
+from AbsGenerator import AbsGenerator
+from utils.utils import createClass
 
-
-class PV(Generator):
+class PV(AbsGenerator):
 
     def __init__(self, configuration, name):
         super().__init__(configuration, name)
+        self.simul = createClass("SIMU_" + name, configuration['Simulate'])
+
+    def doSimula(self, calendar, **kwargs):
+        self.config['calendar'] = kwargs['calendar']
+        return self.simul.simula(self.simul, self.config, kwargs_simulation=kwargs['kwargs_simulation'])
